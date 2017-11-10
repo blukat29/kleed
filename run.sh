396bc5a9f16708071b9df1b82d6946fea8806db4
blukat29 <yunjong@kaist.ac.kr>
2017-11-10 15:21:30 +0000

    Create workspace directory in kleed repo directory

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 docker build -t kleed .
-docker run -v $(pwd):/home/klee/workspace --rm -it kleed /bin/bash
+mkdir -p workspace
+docker run -v $(pwd)/workspace:/home/klee/workspace --rm -it kleed /bin/bash
